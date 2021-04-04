6221cb03b4131ff8f0d184d931b1c911662ddb3d
Theseregrets <rishitashaw49@gmail.com>
2021-04-04 22:17:40 +0530

    commands added

--- app.js
+++ app.js
@@ -1,16 +1,46 @@
 const validator = require("Validator")
 const chalk = require('chalk')
 
-const getNotes = require('./notes.js')
+//const getNotes = require('./notes.js')
 
-const yargs = require('yargs');
+const yargs = require('yargs')
 
-yargs
-    .command({
-        command:'add',
-        describe:'Adding command',
-        handler: argv => {
-            console.log('Adding notes');
-        }
-    })
-    .parse();
+yargs.version('1.1.0')
+
+yargs.command({
+  command: 'add',
+  describe: 'Adding command',
+  builder: {
+    title: {
+      describe: 'Note title',
+      demandOption: true,
+      type: 'string'
+    },
+    body: {
+      describe: 'Note body',
+      demandOption: true,
+      type: 'string'
+    }
+  },
+  handler: function(argv) {
+    console.log('Adding notes');
+    console.log('Title: ' + argv.title)
+    console.log('Body: ' + argv.body)
+  }
+})
+yargs.command({
+  command: 'remove',
+  describe: 'removing command',
+  handler: function() {
+    console.log('removing notes');
+  }
+})
+yargs.command({
+  command: 'read',
+  describe: 'reading command',
+  handler: function() {
+    console.log('reading notes');
+  }
+})
+
+yargs.parse()
